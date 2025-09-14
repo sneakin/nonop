5cd7ff94331e018de56988e12b8e6b98b167c868
Nolan Eakins <sneakin@semanticgap.com>
2025-09-14 18:47:33 -0400

    Open flags.

--- lib/ninep/messages/2000L/open.rb
+++ lib/ninep/messages/2000L/open.rb
@@ -7,6 +7,28 @@ require_relative '../../qid'
 module NineP
   module L2000
     class Topen
+      Flags = {
+        RDONLY:        00000000,
+        WRONLY:        00000001,
+        RDWR:          00000002,
+        NOACCESS:      00000003,
+        CREATE:        00000100,
+        EXCL:          00000200,
+        NOCTTY:        00000400,
+        TRUNC:         00001000,
+        APPEND:        00002000,
+        NONBLOCK:      00004000,
+        DSYNC:         00010000,
+        FASYNC:        00020000,
+        DIRECT:        00040000,
+        LARGEFILE:     00100000,
+        DIRECTORY:     00200000,
+        NOFOLLOW:      00400000,
+        NOATIME:       01000000,
+        CLOEXEC:       02000000,
+        SYNC:          04000000
+      }
+      
       # size[4] Tlopen tag[2] fid[4] flags[4]
       ID = 12
       include Packet::Data
