865abb108caaf7f02a86012f68125c208acd5a78
Nolan Eakins <sneakin@semanticgap.com>
2025-10-21 03:02:32 -0400

    Waits for RemoteFile.

--- lib/nonop/client/remote-file.rb
+++ lib/nonop/client/remote-file.rb
@@ -17,7 +17,7 @@ module NonoP
       @flags = NonoP::OpenFlags.new(flags || :RDONLY)
       @fid = fid || client.next_fid
       @io = RemoteIO.new(client, @fid, path)
-      open(mode: mode, gid: gid, &blk)
+      @open_req = open(mode: mode, gid: gid, &blk)
     end
 
     def client
@@ -30,35 +30,40 @@ module NonoP
 
     def open mode: nil, gid: nil, &blk
       attachment.walk(@path, nfid: @fid) do |pkt|
-        NonoP.vputs { "Walked to #{@flags} #{@flags & :CREATE} #{@path.size} #{pkt.inspect}" }
+        NonoP.vputs { "Walked to #{@path} #{@flags} #{@flags & :CREATE} #{@path.size} #{pkt.inspect}" }
         case pkt
         when Rwalk then
           if pkt.nwqid < @path.size
             client.clunk(@fid) do 
               if @flags & :CREATE
-                create(mode: mode, gid: gid, &blk)
+                create(mode: mode, gid: gid, &blk).wait
               else
-                blk.call(WalkError.new(2, @path.parent(pkt.nwqid, from_top: true)))
+                NonoP.maybe_call(blk, WalkError.new(2, @path.parent(pkt.nwqid, from_top: true)))
               end
-            end
+            end.wait
           else
             client.track_fid(@fid) { self.close }
             client.request(NonoP::L2000::Topen.new(fid: @fid,
                                                    flags: @flags)) do |pkt|
               if ErrorPayload === pkt
-                blk.call(OpenError.new(pkt))
+                NonoP.maybe_call(blk, OpenError.new(pkt))
               else
                 @ready = true
-                blk.call(self)
+                NonoP.maybe_call(blk, self)
               end
-            end
+            end.wait
           end
-        when StandardError then blk.call(pkt)
-        else blk.call(TypeError.new(pkt))
+        when Error then blk ? blk.call(pkt) : raise(pkt)
+        else blk ? blk.call(TypeError.new(pkt)) : raise(TypeError.new(pkt.class.to_s))
         end
       end
     end
 
+    def wait
+      @open_req.wait
+      self
+    end
+    
     def ready?
       @ready
     end
@@ -74,12 +79,12 @@ module NonoP
                                             mode: mode || 0644,
                                             gid: gid || 0)) do |pkt|
             case pkt
-            when ErrorPayload then blk.call(CreateError.new(pkt, path))
-            else blk.call(self)
+            when ErrorPayload then NonoP.maybe_call(blk, CreateError.new(pkt, path))
+            else NonoP.maybe_call(blk, self)
             end
           end
         when ErrorPayload then blk.call(WalkError.new(pkt, path))
-        else raise TypeError.new(pkt)
+        else blk ? blk.call(TypeError.new(pkt)) : raise(TypeError.new(pkt))
         end
       end
     end
