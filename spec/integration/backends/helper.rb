b3125354569fc2ff31ff4d2c962b8144c268893f
Nolan Eakins <sneakin@semanticgap.com>
2025-10-26 23:03:28 -0400

    Default to client integration test driver.

--- spec/integration/backends/helper.rb
+++ spec/integration/backends/helper.rb
@@ -3,7 +3,7 @@ using SG::Ext
 
 require_relative '../../spec-helper'
 
-SPEC_DRIVER = ENV.fetch('DRIVER', 'requests')
+SPEC_DRIVER = ENV.fetch('DRIVER', 'client')
 
 Pathname.new(__FILE__).parent.
   glob("../#{SPEC_DRIVER}/*.rb").each { require_relative(_1) }
